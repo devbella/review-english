5726ce28d5a75a1110c12d31888005d4ac123b69
devbella <bellachoi1210@gmail.com>
2023-11-29 14:32:51 +0900

    Day Page

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,13 +3,17 @@ import englishData from "../englishData.json";
 
 const Main = () => {
   return (
-    <div className="bg-blue-100 min-h-screen max-w-screen-md mx-auto px-8 pt-20">
-      <h1 className="text-center text-2xl font-semibold">Study English</h1>
-      <ul className="mt-12">
-        {englishData.map((v, i) => (
-          <MainCard key={i} title={v.title} day={v.day} />
-        ))}
-      </ul>
+    <div className="bg-green-300">
+      <div className="container">
+        <h1 className="text-4xl font-black underline bg-white rounded-lg border-r-2 border-gray-600 border-b-2 px-4 py-2 inline">
+          Study English ...
+        </h1>
+        <ul className="mt-12 flex flex-col gap-8 pb-20">
+          {englishData.map((v, i) => (
+            <MainCard key={i} title={v.title} day={v.day} />
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
